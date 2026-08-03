bcc0ff0f2da7310dd101f32da58ceae3f28ada5f
kishan <134167588+keysun8@users.noreply.github.com>
2026-08-03 19:10:35 +0530

    Update Blender.py

--- Model/Blender.py
+++ Model/Blender.py
@@ -1,14 +1,17 @@
-# Blender -- FIXED ORDER: horizontal first, then vertical (matches DiffBrush)
+import torch
+import torch.nn as nn
+from .style_encoder import Resblock, SelfAttentionBlock
+from .unet import CrossAttention_unet
 
 class Blender(nn.Module):
     def __init__(self):
         super().__init__()
         self.cross_ver = CrossAttention_unet(64, 64)
-        self.self_ver_1 = SelfAttentionBlock(64)  
-        self.self_ver_2 = SelfAttentionBlock(64)  
+        self.self_ver_1 = SelfAttentionBlock(64)
+        self.self_ver_2 = SelfAttentionBlock(64)
         self.cross_hor = CrossAttention_unet(64, 64)
-        self.self_hor_1 = SelfAttentionBlock(64)  
-        self.self_hor_2 = SelfAttentionBlock(64)  
+        self.self_hor_1 = SelfAttentionBlock(64)
+        self.self_hor_2 = SelfAttentionBlock(64)
         self.res_out = Resblock(64, 64)
 
     def forward(self, Q, S_ver, S_hor):
