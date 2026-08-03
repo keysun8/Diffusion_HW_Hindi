1f444c6c04d841d09cfed3eaf55876e554d0abc4
kishan <134167588+keysun8@users.noreply.github.com>
2026-08-03 19:03:49 +0530

    Add Blender class with attention mechanisms

---
+++ Model/Blender.py
@@ -0,0 +1,22 @@
+# Blender -- FIXED ORDER: horizontal first, then vertical (matches DiffBrush)
+
+class Blender(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cross_ver = CrossAttention_unet(64, 64)
+        self.self_ver_1 = SelfAttentionBlock(64)  
+        self.self_ver_2 = SelfAttentionBlock(64)  
+        self.cross_hor = CrossAttention_unet(64, 64)
+        self.self_hor_1 = SelfAttentionBlock(64)  
+        self.self_hor_2 = SelfAttentionBlock(64)  
+        self.res_out = Resblock(64, 64)
+
+    def forward(self, Q, S_ver, S_hor):
+        cond = self.cross_ver(Q, S_ver)
+        cond = self.self_ver_1(cond)
+        cond = self.self_ver_2(cond)
+        cond = self.cross_hor(cond, S_hor)
+        cond = self.self_hor_1(cond)
+        cond = self.self_hor_2(cond)
+        cond = cond + Q * 0.5
+        return self.res_out(cond)
